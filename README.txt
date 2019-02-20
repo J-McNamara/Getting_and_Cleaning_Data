6fa09ad07212d02a13987b4467c5b8643f5c8fcb
762457 <joshtmcnamara@gmail.com>
2019-02-20 16:11:36 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+This project was completed as a part of the Johns Hopkins Coursera Data Science specialization.
+
+run_analysis.R
+This file transforms an accelerometry dataset into a tidy summary of the means and standard deviations of velocity associated with several activities.
+
+tidy_data.csv
+This is the tidy data output from the run_analysis.R script.
+
+tidy_summary_data.csv
+This is the tidy data output from the run_analysis.R script detailing category averages from different motion activities.
+
+UCI HAR Dataset
+This is the raw data repo used to feed this analysis.
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+# 1. Merges the training and the test sets to create one data set.
+training <- read.table('UCI HAR Dataset/train/X_train.txt')
+training$dataset <- 'training'
+test <- read.table('UCI HAR Dataset/test/X_test.txt')
+test$dataset <- 'test'
+df <- rbind(training,test)
+df2 <- df
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+features <- as.character(read.table('UCI HAR Dataset/features.txt')[,2])
+features[562] <- 'dataset'
+colnames(df) <- features
+means <- as.character(features[grep('mean', features)])
+stds <- append(as.character(features[grep('std', features)]), 'dataset')
+keeps <- append(means, stds)
+df <- subset(df,select=keeps)
+head(df[1,])
+
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(df) <- gsub('^t', 'time_', colnames(df))
+colnames(df) <- gsub('^f', 'frequency_', colnames(df))
+colnames(df) <- gsub('\\(\\)', '', colnames(df))
+colnames(df) <- gsub('-', '_', colnames(df))
+colnames(df) <- gsub('BodyBody', 'Body', colnames(df))
+colnames(df)
+head(df[1,])
+
+# 4. Appropriately labels the data set with descriptive variable names.
+labels <- as.character(read.table('UCI HAR Dataset/activity_labels.txt')[,2])
+descrip_train <- read.table('UCI HAR Dataset/train/y_train.txt')[,1]
+descrip_train <- labels[descrip_train]
+descrip_test <- read.table('UCI HAR Dataset/test/y_test.txt')[,1]
+descrip_test <- labels[descrip_test]
+df$activity <- 'NULL'
+df[df$dataset=='training',]$activity <- descrip_train
+df[df$dataset=='test',]$activity <- descrip_test
+colnames(df)
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+library(dplyr)
+YeOldeSummaryTable <- summarise_each(tbl = group_by(.data = df, activity), funs = 'mean')
+head(YeOldeSummaryTable)
+
+# Bonus - write data to file
+write.csv(x = df, file = 'tidy_data.csv')
+write.csv(x = YeOldeSummaryTable, file = 'tidy_summary_data.csv')
+
+
+
