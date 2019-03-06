08268c6b59bd197155aa8dbcc022c8b0d831143e
762457 <joshtmcnamara@gmail.com>
2019-03-06 16:34:48 -0500

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,21 @@
 # 1. Merges the training and the test sets to create one data set.
+wd <- 'C:\\Users\\Josh\\OneDrive - Johns Hopkins University\\coursera_data_science_program\\03_getting_and_cleaning_data\\course_project'
+setwd(wd) # Lab computer
+
 training <- read.table('UCI HAR Dataset/train/X_train.txt')
+training_subjects <- read.table('UCI HAR Dataset/train/subject_train.txt')
+training_activities <- read.table('UCI HAR Dataset/train/y_train.txt')
 training$dataset <- 'training'
+
 test <- read.table('UCI HAR Dataset/test/X_test.txt')
 test$dataset <- 'test'
-df <- rbind(training,test)
+test_subjects <- read.table('UCI HAR Dataset/test/subject_test.txt')
+test_activities <- read.table('UCI HAR Dataset/test/y_test.txt')
+
+
+df <- rbind(cbind(training_subjects, training, training_activities), cbind(test_subjects, test, test_activities))
+
+#df <- rbind(training,test)
 df2 <- df
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
@@ -36,9 +48,17 @@ df[df$dataset=='training',]$activity <- descrip_train
 df[df$dataset=='test',]$activity <- descrip_test
 colnames(df)
 
+subjects <- 
+
+#You grouped the result by activity only where it should be grouped by subject and activity couple
+# NAs are introduced in last column (called dataset).
+
+
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 library(dplyr)
 YeOldeSummaryTable <- summarise_each(tbl = group_by(.data = df, activity), funs = 'mean')
+YeOldeSummaryTable2 <- summarise_each(tbl = group_by(.data = YeOldeSummaryTable, subject), funs = 'mean')
+
 head(YeOldeSummaryTable)
 
 # Bonus - write data to file
